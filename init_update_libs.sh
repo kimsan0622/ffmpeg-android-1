15d6009e77c74eec3a8a79d77115c6690423c081
san kim <kimsan0622@gmail.com>
2018-11-01 18:10:50 +0900

    change tar xz cmd

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -12,6 +12,6 @@ rm -rf lame-*
 
 wget -O- http://downloads.sourceforge.net/libpng/libpng-1.6.21.tar.xz | tar xJ
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
-wget -O- https://github.com/fribidi/fribidi/archive/0.19.7.tar.gz | tar xj
+wget -O- https://github.com/fribidi/fribidi/archive/0.19.7.tar.gz | tar xz
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
 echo "============================================"
